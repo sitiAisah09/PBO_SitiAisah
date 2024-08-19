0a981283652054b5a3b9ba89fac25b8428fe71db
sitiAisah09 <sitiiaisah64@gmail.com>
2024-08-19 09:51:35 +0700

    Pertemuan 1

---
+++ Latihan 1.5.php
@@ -0,0 +1,30 @@
+<?php
+class Shape
+{
+    function draw()
+    {
+    }
+}
+class Circle extends Shape
+{
+    function draw()
+    {
+        print "Circle has been drawn.</br>";
+
+    }
+}
+class Triangle extends Shape
+{
+    function draw()
+    {
+        print "Triangle has been drawn.</br>";
+    }
+}
+
+class Ellipse extends Shape
+{
+    function draw()
+    {
+        print "Ellipse has been drawn.";
+    }
+}
